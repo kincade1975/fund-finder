98ce789353e134f710b4b19f364425caa84294c0
dblazevic <darko.blazevic@nth.ch>
2015-11-03 14:01:37 +0100

    Etm notification email format changed (via Velocity template)

--- fund-finder/src/main/java/hr/betaware/fundfinder/service/EtmService.java
+++ fund-finder/src/main/java/hr/betaware/fundfinder/service/EtmService.java
@@ -2,20 +2,27 @@ package hr.betaware.fundfinder.service;
 
 import java.io.StringWriter;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
+import etm.contrib.renderer.SimpleHtmlRenderer;
 import etm.core.configuration.BasicEtmConfigurator;
 import etm.core.configuration.EtmManager;
 import etm.core.monitor.EtmMonitor;
@@ -31,6 +38,9 @@ public class EtmService {
 
 	@Autowired
 	private JavaMailSender mailSender;
+
+	@Autowired
+	private VelocityEngine velocityEngine;
 
 	@Value(value = "${etm.notification.enabled}")
 	private boolean notificationEnabled;
@@ -89,18 +99,39 @@ public class EtmService {
 			etmMonitor.render(new SimpleTextRenderer(stringWriter));
 			LOGGER.info("{}\n{}", subject, stringWriter.toString());
 			if (notificationEnabled) {
-				sendEmail(subject, stringWriter);
+				sendEmail(subject);
 			}
 			etmMonitor.reset();
 		}
 	}
 
-	private void sendEmail(String subject, StringWriter stringWriter) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(notificationEmail);
-		message.setSubject(subject);
-		message.setText(stringWriter.toString());
-		mailSender.send(message);
+	private void sendEmail(String subject) {
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				StringWriter stringWriter = new StringWriter();
+				etmMonitor.render(new SimpleHtmlRenderer(stringWriter));
+
+				Map<String, Object> model = new HashMap<String, Object>();
+				model.put("data", stringWriter.toString());
+
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				message.setTo(notificationEmail);
+				message.setSubject(subject);
+				message.setText(VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "email_etm.vm", "UTF-8", model), true);
+			}
+		};
+
+		mailSender.send(preparator);
+
+		//		Map<String, Object> model = new HashMap<String, Object>();
+		//		model.put("data", stringWriter.toString());
+		//
+		//		SimpleMailMessage message = new SimpleMailMessage();
+		//		message.setTo(notificationEmail);
+		//		message.setSubject(subject);
+		//		message.setText(VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "email_etm.vm", "UTF-8", model));
+		//		mailSender.send(message);
 	}
 
 }
